fdddf4b3f33f2c4664f844f54519fd249ed805d6
fangfang.hao <fangfang.hao@honeywell.com>
2020-08-13 15:04:44 +0800

    add graphic size

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { loadModules } from 'esri-loader';
 // import MapView from "esri/views/MapView";
 // import FeatureLayer from 'esri/layers/FeatureLayer';
 import './App.css';
+const STOPS = [
+  { value: 10000, size: 4, label: "<10000" },
+  { value: 20000, size: 8, label: "<20000" },
+  { value: 30000, size: 12, label: "<30000" },
+  { value: 40000, size: 14, label: '>40000' },
+];
 const VisualVariables = [
   {
     type: "size",
@@ -12,14 +18,39 @@ const VisualVariables = [
       title: 'population for city',
     },
     field: "pop2000",
-    stops: [
-      { value: 10000, size: 4, label: "<10000" },
-      { value: 20000, size: 8, label: "<20000" },
-      { value: 30000, size: 12, label: "<30000" },
-      { value: 40000, size: 14, label: '>40000' },
-    ],
+    stops: STOPS,
   }
-]
+];
+const cityLayerRenderer = {
+  type: "simple",
+  symbol: {
+    type: "simple-marker",
+    color: [226, 119, 40], // orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1
+    },
+  },
+  visualVariables: VisualVariables,
+};
+const highlightSymbol = {
+  type: "simple-marker",
+  color: [255, 255, 0],  //yellow
+  outline: {
+    width: 1,
+    color: [0, 255, 255],
+  },
+};
+
+function getSymbolSize(value: number) {
+  for (let stop of STOPS) {
+    if (value < stop.value) {
+      return stop.size;
+    }
+  }
+  return STOPS[STOPS.length - 1].size;
+}
+
 const WebMapView = () => {
   const mapRef = useRef<HTMLDivElement>(null);
 
@@ -27,8 +58,8 @@ const WebMapView = () => {
     () => {
       // lazy load the required ArcGIS API for JavaScript modules and CSS
       loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer',
-        'esri/Graphic', 'esri/layers/GraphicsLayer'], { css: true })
-        .then(([ArcGISMap, MapView, FeatureLayer, Graphic, GraphicsLayer]) => {
+        'esri/Graphic', 'esri/layers/GraphicsLayer', 'esri/widgets/Legend'], { css: true })
+        .then(([ArcGISMap, MapView, FeatureLayer, Graphic, GraphicsLayer, Legend]) => {
           //satellite, streets-relief-vector, light-gray-vector, dark-gray-vector, streets-navigation-vector
           const map = new ArcGISMap({
             basemap: 'streets-navigation-vector'
@@ -38,18 +69,12 @@ const WebMapView = () => {
             //server side databsource
             url: "http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0",
             definitionExpression: "class = 'city'",
-            renderer: {
-              type: "simple",
-              symbol: {
-                type: "simple-marker",
-                color: [226, 119, 40], // orange
-                outline: {
-                  color: [255, 255, 255], // white
-                  width: 1
-                },
-              },
-              visualVariables: VisualVariables,
-            },
+            title: 'Population in US',
+            renderer: cityLayerRenderer,
+            popupTemplate: {
+              title: "{areaname}",
+              content: "{pop2000}",
+            }
           });
           map.add(cityLayer);
 
@@ -66,26 +91,21 @@ const WebMapView = () => {
             },
           });
           map.add(roadLayer);
-          var graphicsLayer = new GraphicsLayer();
+
+          let graphicsLayer = new GraphicsLayer();
           map.add(graphicsLayer);
+
           function addGraphics(result: any) {
             graphicsLayer.removeAll();
             result.features
               .forEach((feature: any) => {
-                // const population = feature.attributes['pop2000'];
-                var g = new Graphic({
+                const population = feature.attributes['pop2000'];
+                let g = new Graphic({
                   geometry: feature.geometry,
                   attributes: feature.attributes,
-                  symbol: {
-                    type: "simple-marker",
-                    color: [255, 255, 0],  //yellow
-                    outline: {
-                      width: 1,
-                      color: [0, 255, 255],
-                    },
-                    // size: `${10 + population / 10000}px`,
-                  },
-                  visualVariables: VisualVariables,
+                  symbol: Object.assign(highlightSymbol, {
+                    size: getSymbolSize(population),
+                  }),
                 });
                 graphicsLayer.add(g);
               });
@@ -103,7 +123,6 @@ const WebMapView = () => {
               where: "class='city'",
             };
             cityLayer.queryFeatures(query).then(function (result: any) {
-              console.log(result.features);
               addGraphics(result);
             });
           }
@@ -113,43 +132,45 @@ const WebMapView = () => {
             container: mapRef.current,
             map: map,
             center: [-118, 34],
-            zoom: 8
+            zoom: 8,
+            highlightOptions: {
+              color: [255, 241, 58],
+              fillOpacity: 0.4
+            },
           });
-          // When the view is ready, call the queryFeatureLayer function and pass in the center of the view, 1500 as the distance in meters, and intersects as the spatial relationship operator to use. This will search for and display features in the center of the map
-          view.when(function () {
-            const query = {
-              geometry: view.center,
-              units: 'kilometers',
-              distance: 50,
-              spatialRelationship: 'intersects',
-              outFields: ['areaname', 'pop2000', 'class'],
-              returnGeometry: false,
-              // outStatistics: true,
-            }
-            // cityLayer.queryFeatures(query).then(function (result: any) {
-            //   // {10 + $feature.pop2000 / 10000}
-            //   const renderer = cityLayer.renderer.clone();
-            //   Object.assign(renderer.symbol, {
-            //     size: '30px',
-            //   });
-            //   cityLayer.renderer = renderer;
-            // });
+          let legend = new Legend({
+            view: view,
+            layerInfos: [{
+              layer: cityLayer,
+              title: 'Legend',
+            }]
+          });
+          view.ui.add(legend, 'top-right');
+
+          view.whenLayerView(roadLayer).then((layerView: any) => {
+            console.log(layerView);
+            layerView.on("click", (value: any) => {
+              // availableFields will become available
+              // once the layer view finishes updating
+              console.log(value);
+            });
           });
 
-          // view.popup.autoOpenEnabled = false;  // Disable the default popup behavior
+
+          view.popup.autoOpenEnabled = false;  // Disable the default popup behavior
           view.on('click', (event: any) => {
             queryFeatureLayer(event.mapPoint, 50, "esriSpatialRelIntersects");
-            // view.hitTest(event)
-            //   .then((hitTestResults: any) => {
-            //     console.log(hitTestResults.results);
-            //     if (hitTestResults.results) {
-            //       // view.popup.open({ // open a popup to show some of the results
-            //       //   location: event.mapPoint,
-            //       //   title: "Hit Test Results",
-            //       //   content: hitTestResults.results.length + "Features Found"
-            //       // });
-            //     }
-            //   })
+            view.hitTest(event)
+              .then((hitTestResults: any) => {
+                console.log(hitTestResults.results);
+                if (hitTestResults.results) {
+                  // view.popup.open({ // open a popup to show some of the results
+                  //   location: event.mapPoint,
+                  //   title: "Hit Test Results",
+                  //   content: hitTestResults.results.length + "Features Found"
+                  // });
+                }
+              })
           })
 
           return () => {
